12ef7808c8c41a4a0130e622fa7af0797b37575c
just-landon <landon.rojas@jpschools.org>
2025-06-16 14:40:08 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(100, 650, 120, 20, "green");
+    createPlatform(300, 550, 150, 25, "blue");
+    createPlatform(600, 450, 100, 20, "orange");
+    createPlatform(900, 350, 200, 30, "purple");
+    createPlatform(1200, 250, 130, 15, "yellow");
+
 
 
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("grace", 400, 500, 0.3, 0.5);
+    createCollectable("kennedi", 790, 350, 0, 0);
+    createCollectable("max", 1100, 250, 0, 0);
+    createCollectable("database", 1250, 150, 0, 0)
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
+    createCannon("top", 200, 1000);
+    createCannon("right", 300, 2000);
+    createCannon("bottom", 700, 3000)
+    createCannon("left", 490, 2000)
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
