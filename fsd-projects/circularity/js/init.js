0843e59daa32e7d35c819793ed7c327871b10d48
just-landon <landon.rojas@jpschools.org>
2025-06-25 15:37:25 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -34,11 +34,11 @@ var init = function (window) {
 
 
         // TODO 3 : Call the drawCircle() function
-        drawCircle ();
-        drawCircle ();
-        drawCircle ();
-        drawCircle ();
-        drawCircle ();
+        //drawCircle ();
+        //drawCircle ();
+        //drawCircle ();
+        //drawCircle ();
+        //drawCircle ();
         // TODO 7 : Use a loop to create multiple circles
         for (var i = 0; i < 100; i++) {
             drawCircle();
@@ -57,18 +57,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            //physikz.updatePosition(circles[0]);
+            //physikz.updatePosition(circles[1]);
+            //physikz.updatePosition(circles[2]);
+            //physikz.updatePosition(circles[3]);
+            //physikz.updatePosition(circles[4]);
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
+            //game.checkCirclePosition(circles[0]);
+            //game.checkCirclePosition(circles[1]);
+            //game.checkCirclePosition(circles[2]);
+            //game.checkCirclePosition(circles[3]);
+            //game.checkCirclePosition(circles[4]);
 
             // TODO 8 / TODO 9 : Iterate over the array
             for (var i = 0; i < 100; i++) {
